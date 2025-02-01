a0a3243ac9634d9f8affb735460cd2501b8f71c9
eci4ever <eci4ever@gmail.com>
2025-02-01 22:12:39 +0800

    add Pinia store integration to main application setup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './routers/index'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+
+createApp(App)
+.use(router)
+.use(pinia)
+.mount('#app')
